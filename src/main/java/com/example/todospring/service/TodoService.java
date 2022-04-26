1bdef40e509ce9640d17afc3c753b2065d7b3a5f
Joonwoo Park <93jpark@gmail.com>
2022-04-26 15:44:30 +0900

    write TodoService to handle service logic

--- src/main/java/com/example/todospring/service/TodoService.java
+++ src/main/java/com/example/todospring/service/TodoService.java
@@ -1,10 +1,26 @@
 package com.example.todospring.service;
 
+import com.example.todospring.model.TodoEntity;
+import com.example.todospring.persistance.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
+    @Autowired
+    private TodoRepository repository;
+
     public String testService() {
-        return "Test Service";
+        // TodoEntity 생성
+        TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+
+        // TodoEntity 저장
+        repository.save(entity);
+
+        // TodoEntity 검색
+        TodoEntity savedEntity = repository.findById(entity.getId()).get();
+
+        return savedEntity.getTitle();
+
     }
 }
