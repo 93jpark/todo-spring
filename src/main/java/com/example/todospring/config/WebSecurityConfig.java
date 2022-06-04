ba2cd009ed4f6c72c15f2bdc9ef50e7f644d6055
Joonwoo Park <93jpark@gmail.com>
2022-06-04 14:27:40 +0900

    implements WebSecurityConfig class for WebSecurityConfigurerAdapter

--- src/main/java/com/example/todospring/config/WebSecurityConfig.java
+++ src/main/java/com/example/todospring/config/WebSecurityConfig.java
@@ -1,2 +1,41 @@
-package com.example.todospring.config;public class WebSecurityConfig {
+package com.example.todospring.config;
+
+import com.example.todospring.security.JwtAuthenticationFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
+
+@EnableWebSecurity
+@Slf4j
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // http 시큐리티 빌더
+        http.cors()
+                .and()
+                .csrf().disable() // csrf는 현재 사용하지 않음으로 disable
+                .httpBasic().disable() // token을 사용함으로 basic 인증 disable
+                .sessionManagement() // session 기반이 아님을 선언
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests() // /와 /auth/** 경로는 인증 안해도 됨
+                .antMatchers("/", "/auth/**").permitAll()
+                .anyRequest() // /와 /auth/** 이외의 모든 경로는 인증 해야 됨
+                .authenticated();
+        // filter 등록
+        // 매 요청마다
+        // CorsFilter 실행한 후에
+        // jwtAuthenticationFilter 실행한다
+        http.addFilterAfter(
+                jwtAuthenticationFilter,
+                CorsFilter.class
+        );
+    }
 }
