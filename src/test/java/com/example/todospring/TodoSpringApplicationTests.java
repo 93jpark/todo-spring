23eff18f87e7372b03016ae802fe7d84741a4d1e
Joonwoo Park <93jpark@gmail.com>
2022-06-21 15:36:53 +0900

    fixed test issue

--- src/test/java/com/example/todospring/TodoSpringApplicationTests.java
+++ src/test/java/com/example/todospring/TodoSpringApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.todospring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@WebAppConfiguration
 @SpringBootTest
 class TodoSpringApplicationTests {
 
